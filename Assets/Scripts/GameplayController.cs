b41e58305ed2120e6b5255350e4936b7117b5249
linzhaoqian <1203783169@qq.com>
2021-11-25 00:04:57 +0800

    11.25

---
+++ Assets/Scripts/GameplayController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameplayController : MonoBehaviour
+{
+
+    public string[] queue;
+    public GameObject[] platforms;
+
+    public int j;
+    private bool isWaitingForChangeColor;
+
+    private List<string> poems = new List<string>();
+
+    private Color colorRed = new Vector4(0.8f, 0, 0, 1f);
+    private Color colorBlack = new Vector4(0f, 0f, 0f, 0f);
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        j = 0;
+        isWaitingForChangeColor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeColor();
+    }
+    private void ChangeColor()
+    {
+        if (isWaitingForChangeColor)
+        {
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                if (platforms[i].tag == queue[j])
+                {
+                    platforms[i].GetComponentInChildren<SpriteRenderer>().color = colorRed;
+                    platforms[i].GetComponentInChildren<Text>().color = Color.white;
+                }
+                else
+                {
+                    platforms[i].GetComponentInChildren<SpriteRenderer>().color = Color.white;
+                    platforms[i].GetComponentInChildren<Text>().color = Color.black;
+                }
+            }
+            isWaitingForChangeColor = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (j <= queue.Length)
+        {
+            if (coll.gameObject.tag == queue[j])
+            {
+                isWaitingForChangeColor = true;
+                poems.Add(queue[j]);
+                j++;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+
+    public float speed, jumpForce;
+    public Transform groundCheck;
+    public LayerMask ground;
+
+    public  bool isGround,isJump;
+    public int jumpAbility;
+    bool jumpPressed;
+    
+    int jumpCount;
+    // Start is called before the first frame update
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump")&&jumpCount >0)
+        {
+            jumpPressed =true;
+        }
+    }
+    private void FixedUpdate() 
+    {
+        isGround =Physics2D.OverlapCircle(groundCheck.position,0.1f,ground);
+
+        GroundMovement();
+        Jump();
+    }
+    void GroundMovement()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        rb .velocity =new Vector2(horizontalMove*speed,rb .velocity.y);
+        if(horizontalMove!=0)
+        {
+           transform .localScale =new Vector3(horizontalMove,1,1); 
+        }
+    }
+
+    void Jump()
+    {
+        if(isGround)
+        {
+            jumpCount =jumpAbility;
+            isJump = false;
+        }
+        if(jumpPressed && isGround)
+        {
+            isJump = true;
+            rb.velocity =new Vector2(rb.velocity.x,jumpForce);
+            jumpCount--;
+            jumpPressed =false;
+        }   
+        else if(jumpPressed&&jumpCount>0 && isJump)
+        {
+            rb.velocity =new Vector2(rb.velocity.x,jumpForce);
+            jumpCount --;
+            jumpPressed =false;
+        }
+    }
+}
